57f3d72ce49afbad1684db5e9fe883fd73cbefef
vinothkumark <vinothkumar.k83@gmail.com>
2024-12-10 11:52:25 +0530

    API changes to fix deployment issue

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,16 +1,11 @@
-import axios from "axios";
+import { data } from "@/app/lib/projects/projects";
 import { NextResponse } from "next/server";
-import {data} from "@/app/lib/projects/projects";
 
 export async function GET() {
-  const BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN;
-
   try {
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
-    console.error(error);
-    return {
-      notFound: true,
-    };
+    console.error("Error fetching data:", error);
+    return NextResponse.json({ message: "Not Found" }, { status: 404 });
   }
 }
